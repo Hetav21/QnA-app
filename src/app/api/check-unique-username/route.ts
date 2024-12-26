29fc1a827e022b063841bcf4815110cf4fd59f03
Hetav21 <shahhetav2106@gmail.com>
2024-12-26 17:05:14 +0530

    Forgot to add comments xD

--- src/app/api/check-unique-username/route.ts
+++ src/app/api/check-unique-username/route.ts
@@ -4,34 +4,38 @@ import { UserModel } from "@/model/User";
 import { usernameValidation } from "@/schemas/signUpSchema";
 
 export async function GET(request: Request) {
+  // Waiting for db connection
   await dbConnect();
 
   try {
+    // Extracting username from query params
     const { searchParams } = new URL(request.url);
-
     const username = searchParams.get("username");
 
+    // Validating username
     const result = usernameValidation.safeParse(username);
 
+    // If username is invalid
     if (!result.success) {
       const errors = result.error.format()._errors || [];
-
-      console.log(errors);
 
       return response(
         {
           success: false,
+          // If there are errors, join them, else show default message
           message: errors?.length > 0 ? errors.join(", ") : "Invalid username",
         },
         400,
       );
     }
 
+    // Checking if verified username already exists
     const existingVerifiedUser = await UserModel.findOne({
       username,
       isVerified: true,
     });
 
+    // Username already taken
     if (existingVerifiedUser) {
       return response(
         {
@@ -42,6 +46,7 @@ export async function GET(request: Request) {
       );
     }
 
+    // Username is available
     return response(
       {
         success: true,
--- src/app/api/verify-code/route.ts
+++ src/app/api/verify-code/route.ts
@@ -4,20 +4,24 @@ import { UserModel } from "@/model/User";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
+  // Waiting for db connection
   await dbConnect();
 
   try {
+    // Extracting username and code from request body
     const body = await req.json();
-
     const username = body.username;
     const code = body.code;
 
+    // decoding username from URI
     const decodedUsername = decodeURIComponent(username);
 
+    // Finding user from db
     const user = await UserModel.findOne({
       username: decodedUsername,
     });
 
+    // If user not found
     if (!user) {
       return response(
         {
@@ -28,20 +32,22 @@ export async function POST(req: NextRequest) {
       );
     }
 
+    // If user is already verified
     if (user.isVerified) {
       return response(
         {
           success: false,
           message: "User already verified",
         },
-        400,
+        200,
       );
     }
 
+    // Checking if code is correct and not expired
     const isCodeValid = user.verifyCode === code;
-
     const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
 
+    // If code is correct and not expired
     if (isCodeValid && isCodeNotExpired) {
       user.isVerified = true;
       await user.save();
@@ -54,6 +60,7 @@ export async function POST(req: NextRequest) {
         200,
       );
     } else if (!isCodeNotExpired) {
+      // Code expired
       return response(
         {
           success: false,
@@ -63,6 +70,7 @@ export async function POST(req: NextRequest) {
         400,
       );
     } else {
+      //  Code is incorrect
       return response(
         {
           success: false,
