4c3a757687f3e0ba11c7826b52d47efc661b7612
Hetav21 <shahhetav2106@gmail.com>
2024-12-29 19:36:13 +0530

    Adding navbar and changing middleware for smooth login and logout

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { useToast } from "@/hooks/use-toast";
+import { Label } from "./ui/label";
+
+export function NavBar() {
+  // Extracting session information
+  const { data: session } = useSession();
+
+  // const user = session?.user;
+
+  // Initializing toast
+  const { toast } = useToast();
+
+  return (
+    <nav className="p-4 md:p-6 shadow-md">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+        <Link
+          className="text-2xl font-bold mb-4 md:mb-0"
+          // Session based redirection
+          href={session ? "/dashboard" : "/"}
+        >
+          Anonymous Feedback
+        </Link>
+        {
+          // Session based rendering of Login or Logout
+          session ? (
+            <>
+              {/* <span className="mr-4">
+              Welcome, {user?.username || user?.email}
+            </span> */}
+              <Button
+                className="w-full md:w-auto"
+                onClick={() => {
+                  toast({
+                    title: "Logged out",
+                    description: "Logged out successfully",
+                    variant: "default",
+                  });
+                  signOut();
+                }}
+              >
+                <Label>Logout</Label>
+              </Button>
+            </>
+          ) : (
+            <Link href="/sign-in">
+              <Button className="w-full md:w-auto">
+                <Label>Login</Label>
+              </Button>
+            </Link>
+          )
+        }
+      </div>
+    </nav>
+  );
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -6,14 +6,18 @@ export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
 
-  // If user is logged in and trying to access [sign-in, sign-up, verify, /], redirect to dashboard
+  // If user is logged in and trying to access [sign-in, sign-up, verify], redirect to dashboard
   if (
     token &&
     (url.pathname.startsWith("/sign-in") ||
       url.pathname.startsWith("/sign-up") ||
-      url.pathname.startsWith("/verify"))
+      url.pathname.startsWith("/verify") ||
+      url.pathname === "/")
   ) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
+  } else if (!token && url.pathname.startsWith("/dashboard")) {
+    // If user is not logged in and trying to access the site, redirect to /sign-in
+    return NextResponse.redirect(new URL("/sign-in", request.url));
   }
 }
 
