006b4295554fd5604f08ab87859df0aa1c95766f
Hetav21 <shahhetav2106@gmail.com>
2024-12-29 17:51:57 +0530

    Completing sign in page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,26 +1,155 @@
 "use client";
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useState } from "react";
+import { useToast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
 
-// Dummy sign-in page for testing
-export default function Component() {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Loader2 } from "lucide-react";
+import { signInSchema } from "@/schemas/signInSchema";
+import { signIn } from "next-auth/react";
+import { Label } from "@/components/ui/label";
+
+const SignInPage = () => {
+  // States to block events from happening while checking username and submitting form
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  // Creating toasts
+  const { toast } = useToast();
+
+  // To navigate to the verify page after successful sign up
+  const router = useRouter();
+
+  // Custom form with username, email and password
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: "",
+      password: "",
+    },
+  });
+
+  // To handle on submit events
+  const onSubmit: SubmitHandler<z.infer<typeof signInSchema>> = async (
+    data,
+  ) => {
+    setIsSubmitting(true);
+
+    const result = await signIn("credentials", {
+      identifier: data.identifier,
+      password: data.password,
+      redirect: false,
+    });
+
+    if (result?.error) {
+      if (result.error == "CredentialSignin") {
+        toast({
+          title: "Error Signing in",
+          description: "Invalid email or password",
+          variant: "destructive",
+        });
+      } else {
+        toast({
+          title: "Error Signing in",
+          description: result.error,
+          variant: "destructive",
+        });
+      }
+    }
+
+    if (result?.url) {
+      toast({
+        title: "Success",
+        description: "Successfully signed in",
+        variant: "default",
+      });
+      router.replace("/dashboard");
+    }
+
+    setIsSubmitting(false);
+  };
+
   return (
-    <>
-      Not signed in <br />
-      <button
-        className="bg-orange-500 px-3 py-1 m-4 rounded"
-        onClick={() => signIn()}
-      >
-        Sign in
-      </button>
-    </>
+    <div className="flex justify-center items-center min-h-screen bg-zinc-200">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Welcome Back
+          </h1>
+          <p className="mb-4">Sign in to continue your anonymous adventure</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              name="identifier"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email/Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="email/username" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    <Label className="font-medium text-sm">
+                      Enter your registered email address or username
+                    </Label>
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="***********"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" disabled={isSubmitting}>
+              {isSubmitting ? (
+                <Loader2 className="mr-2 h-4 w-4 animate-spin">
+                  Please Wait
+                </Loader2>
+              ) : (
+                "Sign In"
+              )}
+            </Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+          <p>
+            Not a member?{" "}
+            <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
   );
-}
+};
+
+export default SignInPage;
