fbf37fb7e6b1ec310eca9183d82816a3072adea5
Hetav21 <shahhetav2106@gmail.com>
2024-12-30 20:28:32 +0530

    Completed basic user page

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -1,4 +1,105 @@
-// Dummy page for feedback
-export default function Page() {
-  return <div>Feedback page</div>;
+"use client";
+
+import { Label } from "@/components/ui/label";
+import { useToast } from "@/hooks/use-toast";
+import { messageSchema } from "@/schemas/messageSchema";
+import { ApiResponse } from "@/types/ApiResponse";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios, { AxiosResponse } from "axios";
+import { useParams } from "next/navigation";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+
+export default function UserPage() {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const params = useParams<{ username: string }>();
+
+  const { toast } = useToast();
+
+  const form = useForm<z.infer<typeof messageSchema>>({
+    resolver: zodResolver(messageSchema),
+    defaultValues: {
+      content: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<z.infer<typeof messageSchema>> = async (
+    data,
+  ) => {
+    setIsSubmitting(true);
+
+    const result: AxiosResponse<ApiResponse> = await axios.post(
+      "/api/send-message",
+      {
+        username: params.username,
+        content: data.content,
+      },
+    );
+
+    if (result.data.success) {
+      toast({
+        title: "Message sent successfully",
+        description: "Your message has been sent successfully",
+        variant: "default",
+      });
+    } else {
+      toast({
+        title: "Error sending message",
+        description: result.data.message,
+        variant: "destructive",
+      });
+    }
+
+    setIsSubmitting(false);
+  };
+
+  return (
+    <div className="flex justify-center items-center">
+      <div className="mx-28 w-full p-8 bg-white rounded-lg">
+        <div className="flex-grow flex flex-col items-center justify-center md:px-12 py-6 text-black">
+          <section className="text-center mb-8 md:mb-12">
+            <h1 className="text-xl md:text-4xl font-bold">
+              Send Feedback Annonymously
+            </h1>
+          </section>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="content"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="pl-1 font-bold text-lg">
+                    Send message to u/{params.username}
+                  </FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Enter your message here"
+                      className="w-full"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Send message now!!</Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
 }
