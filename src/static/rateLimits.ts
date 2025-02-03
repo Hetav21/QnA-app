56b06253cf72d9b9d0d658136e6dc8bfbb4074f9
Hetav21 <shahhetav2106@gmail.com>
2025-02-04 00:53:33 +0530

    Changing rate limits

--- src/static/rateLimits.ts
+++ src/static/rateLimits.ts
@@ -1,13 +1,13 @@
 // Contains Rate Limits for different routes
 
 // Rate Limit for /verify-code
-export const verifyCodeRL = 5;
+export const verifyCodeRL = 4;
 
 // Rate Limit for /sign-up
-export const signUpRL = 10;
+export const signUpRL = 5;
 
 // Rate Limit for /send-message
 export const sendMessageRL = 5;
 
 // Rate Limit for /suggest-messages
-export const suggestMessagesRL = 5;
+export const suggestMessagesRL = 4;
